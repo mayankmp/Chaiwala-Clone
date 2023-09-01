0646a371fe63dcdb162c385f702c8ac9e7d7f09e
Mayank Patel <134678339+mayankmp@users.noreply.github.com>
2023-09-01 14:38:23 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -118,6 +118,7 @@ const Footer = () => {
         <div>
           <h5>Phone</h5>
           <a href="tel: +917611106886">+917611106886</a>
+          <a href="tel: +919993739771">+919993739771</a>
         </div>
 
         <p>© COPYRIGHT 2022 MBA CHAIWALA. MADE BY MAYANK PATEL & DIVYANK MISHRA</p>
